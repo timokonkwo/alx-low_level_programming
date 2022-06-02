af78386a2d55aba6fc6c83e58ca82482e02dbb86
Tim Okonkwo <chibuiketimokonkwo@gmail.com>
2022-06-02 16:53:38 -0700

    alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
- * Random betty comment
+ *main function Random betty comment
 */
 int main(void)
 {
 char mine[] = "\"Programming is like building a multilingual puzzle";
 puts(mine);
+/**
+ * return 
+*/
 return (0);
 }
