a33e044b5c9b3a00bbc66a8f306f77c9bf728532
Tim Okonkwo <chibuiketimokonkwo@gmail.com>
2022-06-02 17:34:53 -0700

    alx

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@ int intType;
 long int longInt;
 long long int longerInt;
 float floatType;
-printf("Size of char: %zu byte\n", sizeof(charType));
-printf("Size of int: %zu byte\n", sizeof(intType));
-printf("Size of char: %zu byte(s)\n", sizeof(longInt));
-printf("Size of char: %zu byte(s)\n", sizeof(longerInt));
+printf("Size of char: %zu byte(s)\n", sizeof(charType));
+printf("Size of int: %zu byte(s)\n", sizeof(intType));
+printf("Size of long int: %zu byte(s)\n", sizeof(longInt));
+printf("Size of long long int: %zu byte(s)\n", sizeof(longerInt));
 printf("Size of float: %zu byte(s)\n", sizeof(floatType));		    return (0);
 }
