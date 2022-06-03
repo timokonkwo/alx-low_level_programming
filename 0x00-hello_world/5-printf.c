da42e5db1e91f7653083a239200703d8a244516b
Tim Okonkwo <chibuiketimokonkwo@gmail.com>
2022-06-02 17:04:23 -0700

    alx

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main function Random betty comment
+*/
+
+int main(void)
+{
+char mine[] = "\"Programming is like building a multilingual puzzle";
+
+printf("%s",mine);
+return (0);
+}
